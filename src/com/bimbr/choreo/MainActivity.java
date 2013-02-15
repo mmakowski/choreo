fc11b369d11fd775dcc9b3d0b32bc73bc9dd93ec
Maciek Makowski <maciek.makowski@gmail.com>
2013-02-15 22:34:20 +0000

    audio player controls appear, but hide automatically and don't come back on touch

--- src/com/bimbr/choreo/MainActivity.java
+++ src/com/bimbr/choreo/MainActivity.java
@@ -2,16 +2,15 @@ package com.bimbr.choreo;
 
 import java.io.IOException;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.provider.MediaStore;
-import android.app.Activity;
-import android.content.Intent;
-import android.database.Cursor;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.MediaController;
@@ -19,155 +18,155 @@ import android.widget.MediaController.MediaPlayerControl;
 import android.widget.ScrollView;
 
 public class MainActivity extends Activity {
-	private static final int SELECT_MUSIC_REQUEST_CODE = 1;
+    private static final int SELECT_MUSIC_REQUEST_CODE = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         promptForMusic();
     }
 
-	private void startMediaPlayer(String selectedAudioPath) {
-		final ScrollView choreoView = (ScrollView) findViewById(R.id.choreoView);        
+    private void startMediaPlayer(final String selectedAudioPath) {
+        final ScrollView choreoView = (ScrollView) findViewById(R.id.choreoView);
 
         final MediaController mediaController = new MediaController(this);
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        mediaPlayer.setOnPreparedListener(new OnPreparedListener(){
-			@Override
-			public void onPrepared(MediaPlayer mp) {
-			    mediaController.setMediaPlayer(new MediaPlayerControl(){
+        final MediaPlayer mediaPlayer = new MediaPlayer();
 
-					@Override
-					public void start() {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public void pause() {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public int getDuration() {
-						// TODO Auto-generated method stub
-						return 0;
-					}
-
-					@Override
-					public int getCurrentPosition() {
-						// TODO Auto-generated method stub
-						return 0;
-					}
-
-					@Override
-					public void seekTo(int pos) {
-						// TODO Auto-generated method stub
-						
-					}
-
-					@Override
-					public boolean isPlaying() {
-						// TODO Auto-generated method stub
-						return false;
-					}
-
-					@Override
-					public int getBufferPercentage() {
-						// TODO Auto-generated method stub
-						return 0;
-					}
-
-					@Override
-					public boolean canPause() {
-						// TODO Auto-generated method stub
-						return false;
-					}
-
-					@Override
-					public boolean canSeekBackward() {
-						// TODO Auto-generated method stub
-						return false;
-					}
-
-					@Override
-					public boolean canSeekForward() {
-						// TODO Auto-generated method stub
-						return false;
-					}});
-			    mediaController.setAnchorView(choreoView);
-
-			    new Handler().post(new Runnable() {
-			      public void run() {
-			        mediaController.setEnabled(true);
-			        mediaController.show();
-			      }
-			    });				
-			}});
+        mediaPlayer.setOnPreparedListener(new OnPreparedListener() {
+            @Override
+            public void onPrepared(final MediaPlayer player) {
+                mediaController.setMediaPlayer(new ForwardingMediaPlayerControl(player));
+                mediaController.setAnchorView(choreoView);
+                mediaController.setEnabled(true);
+                mediaController.show(0);
+                //                new Handler().post(new Runnable() {
+                //                    @Override
+                //                    public void run() {
+                //                        mediaController.setEnabled(true);
+                //                        mediaController.show();
+                //                    }
+                //                });
+            }});
 
         try {
-          mediaPlayer.setDataSource(selectedAudioPath);
-          mediaPlayer.prepare();
-          mediaPlayer.start();
-          Log.d("AudioPlayer", "started playing " + selectedAudioPath);
-        } catch (IOException e) {
-          Log.e("AudioPlayer", "Could not open file " + selectedAudioPath + " for playback.", e);
+            mediaPlayer.setDataSource(selectedAudioPath);
+            mediaPlayer.prepare();
+            Log.d("AudioPlayer", "prepared " + selectedAudioPath);
+        } catch (final IOException e) {
+            Log.e("AudioPlayer", "Could not open file " + selectedAudioPath + " for playback.", e);
         }
-	}
+    }
 
-	private void promptForMusic() {
-		String path = Environment.getExternalStorageDirectory().getAbsolutePath();
-        Intent musicSelection = new Intent(path);
+    private void promptForMusic() {
+        final String path = Environment.getExternalStorageDirectory().getAbsolutePath();
+        final Intent musicSelection = new Intent(path);
         musicSelection.setType("audio/mp3");
         musicSelection.setAction(Intent.ACTION_GET_CONTENT);
         musicSelection.addCategory(Intent.CATEGORY_OPENABLE);
         startActivityForResult(Intent.createChooser(musicSelection, "select music"), SELECT_MUSIC_REQUEST_CODE);
-	}
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(final Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         // getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
-    
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == RESULT_OK) {
-			switch (requestCode) {
-			case SELECT_MUSIC_REQUEST_CODE:
-				Uri selectedAudioUri = data.getData();
-				String selectedAudioPath = getPathAudio(selectedAudioUri);
-		        startMediaPlayer(selectedAudioPath);
-				break;
-			}
-		}
-	}
-	
-	private String getPathAudio(Uri uriAudio) {
-		// String selectedImagePath;
-		// 1:MEDIA GALLERY --- query from MediaStore.Images.Media.DATA
-		String selectedAudioPath = "";
-		String[] projection = { MediaStore.Audio.Media.DATA };
 
-		Cursor cursor = managedQuery(uriAudio, projection, null, null, null);
+    @Override
+    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+            case SELECT_MUSIC_REQUEST_CODE:
+                final Uri selectedAudioUri = data.getData();
+                final String selectedAudioPath = getPathAudio(selectedAudioUri);
+                startMediaPlayer(selectedAudioPath);
+                break;
+            }
+        }
+    }
 
-		if (cursor != null) {
-			int column_index = cursor
-					.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
-			cursor.moveToFirst();
-			selectedAudioPath = cursor.getString(column_index);
+    private String getPathAudio(final Uri uriAudio) {
+        // String selectedImagePath;
+        // 1:MEDIA GALLERY --- query from MediaStore.Images.Media.DATA
+        String selectedAudioPath = "";
+        final String[] projection = { MediaStore.Audio.Media.DATA };
 
-		} else {
-			selectedAudioPath = null;
-		}
+        final Cursor cursor = managedQuery(uriAudio, projection, null, null, null);
 
-		if (selectedAudioPath == null) {
-			selectedAudioPath = uriAudio.getPath();
-		}
+        if (cursor != null) {
+            final int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+            cursor.moveToFirst();
+            selectedAudioPath = cursor.getString(column_index);
 
-		return selectedAudioPath;
-	}
+        } else {
+            selectedAudioPath = null;
+        }
 
+        if (selectedAudioPath == null) {
+            selectedAudioPath = uriAudio.getPath();
+        }
+
+        return selectedAudioPath;
+    }
+
+    private static final class ForwardingMediaPlayerControl implements MediaPlayerControl {
+        private final MediaPlayer player;
+
+        private ForwardingMediaPlayerControl(final MediaPlayer player) {
+            this.player = player;
+        }
+
+        @Override
+        public void start() {
+            player.start();
+        }
+
+        @Override
+        public void pause() {
+            player.pause();
+        }
+
+        @Override
+        public int getDuration() {
+            return player.getDuration();
+        }
+
+        @Override
+        public int getCurrentPosition() {
+            return player.getCurrentPosition();
+        }
+
+        @Override
+        public void seekTo(final int pos) {
+            player.seekTo(pos);
+
+        }
+
+        @Override
+        public boolean isPlaying() {
+            return player.isPlaying();
+        }
+
+        @Override
+        public int getBufferPercentage() {
+            return 100;
+        }
+
+        @Override
+        public boolean canPause() {
+            return true;
+        }
+
+        @Override
+        public boolean canSeekBackward() {
+            return false;
+        }
+
+        @Override
+        public boolean canSeekForward() {
+            return false;
+        }
+    }
 }
