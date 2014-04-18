14d6d6ba2ed046c8c473a98a27bac5d97b5f8867
Maciek Makowski <maciek.makowski@gmail.com>
2014-04-18 18:26:41 +0100

    first passing test

--- src/main/scala/com/bimbr/choreo/model/Choreography2.scala
+++ src/main/scala/com/bimbr/choreo/model/Choreography2.scala
@@ -1,11 +1,21 @@
 package com.bimbr.choreo.model
 
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration._
 
 case class Choreography2(name: String,
                          length: Duration,
-                         events: Map[Duration, Event] = Map.empty) {
-  def setTempo(time: Duration, measuresPerMinute: Int) = insertEvent(TempoChange(time, measuresPerMinute))
+                         events: Seq[Event] = Seq.empty) {
+  def setTempo(time: Duration, measuresPerMinute: Int): Choreography2 = {
+    //insertEvent(TempoChange(time, measuresPerMinute))
+    recalculateMeasures(measuresPerMinute, timeFrom = time)
+  }
 
-  private def insertEvent(event: Event) = copy(events = events + (event.time -> event))
+  private def insertEvent(event: Event): Choreography2 = copy(events = events :+ event)
+
+  private def recalculateMeasures(measuresPerMinute: Int, timeFrom: Duration): Choreography2 = {
+    val measureLength = (1000 * 60 / measuresPerMinute).milliseconds
+    val measureTimesMs = timeFrom.toMillis.to(length.toMillis, step = measureLength.toMillis)
+    val measureEvents = measureTimesMs.map(t => Measure(t.milliseconds))
+    measureEvents.foldLeft(this)((c, m) => c.insertEvent(m))
+  }
 }
--- src/test/scala/com/bimbr/choreo/model/ChoreographySpec.scala
+++ src/test/scala/com/bimbr/choreo/model/ChoreographySpec.scala
@@ -6,9 +6,16 @@ import scala.concurrent.duration._
 
 class ChoreographySpec extends Specification with NoTimeConversions {
   "A Choreography" should {
-    "contain measure events once tempo is set" in {
+    "contain measure events at appropriate time intervals once tempo is set" in {
+      val tempoBpm = 120
+      val tempoBps = tempoBpm / 60
+      val msPerMeasure = 1000 / tempoBps
       val choreo = new Choreography2("test", 10.seconds).setTempo(0.seconds, 120)
-      choreo.events must not be empty // TODO: proper test
+      val expectedMeasureTimes = (0 to (choreo.length.toSeconds.toInt * tempoBps)).map(t => (t * msPerMeasure).milliseconds)
+      val actualMeasureTimes = choreo.events.collect {
+        case Measure(t) => t
+      }
+      actualMeasureTimes mustEqual expectedMeasureTimes
     }
   }
 }
